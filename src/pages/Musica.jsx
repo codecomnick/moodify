43fedb3bd706defde32c113c0c1d35b609caa62e
Raíssa Alves Donato <rah24@Raissas-MacBook-Air.local>
2025-07-13 16:31:27 -0300

    feat: pegar musicas pela api do itunes com base no humor e, ao clicar, redirecionar para respectiva musica

--- src/pages/Musica.jsx
+++ src/pages/Musica.jsx
@@ -1,16 +1,54 @@
 import './Musica.css';
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+const moodMap = {
+  relaxing: "relaxing",
+  angry: "angry",
+  happy: "happy",
+  energetic: "energetic",
+  sad: "sad",
+};
 
 const Musica = () => {
   const navigate = useNavigate();
-  const [musicaAberta, setMusicaAberta] = useState(null);
+  const location = useLocation();
+
+  const humor = location.state?.humor || "happy";
+
+  const [musicas, setMusicas] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const toggleMusica = (index) => {
-    setMusicaAberta(musicaAberta === index ? null : index);
+    window.open(index)
   };
 
-  const musicas = ['Música 1', 'Música 2', 'Música 3', 'Música 4'];
+  const buscarMusicas = async (humor) => {
+    const termo = moodMap[humor] || "music";
+    setLoading(true);
+
+    try {
+      const response = await fetch(
+        `https://itunes.apple.com/search?term=${encodeURIComponent(
+          termo
+        )}&limit=5&media=music`
+      );
+      const data = await response.json();
+      setMusicas(data.results);
+    } catch (error) {
+      console.error("Erro ao buscar músicas:", error);
+      setMusicas([]);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    buscarMusicas(humor);
+  }, [humor]);
+
+  console.log(musicas);
+
 
   return (
     <div className="home">
@@ -31,21 +69,23 @@ const Musica = () => {
             </h1>
           </div>
 
-          {musicas.map((musica, index) => (
-            <div
-              key={index}
-              className={`accordion-item ${
-                musicaAberta === index ? 'active' : ''
-              }`}
-            >
-              <button
-                className="accordion-header"
-                onClick={() => toggleMusica(index)}
+          {loading ? (
+            <p>Carregando...</p>
+          ) : (
+            musicas.map((musica, index) => (
+              <div
+                key={index}
+                className={`accordion-item`}
               >
-                {musica}
-              </button>
-            </div>
-          ))}
+                <button
+                  className="accordion-header"
+                  onClick={() => toggleMusica(musica.trackViewUrl)}
+                >
+                  {`${musica.trackName} - ${musica.artistName}`}
+                </button>
+              </div>
+            ))
+          )}
         </div>
       </main>
     </div>
